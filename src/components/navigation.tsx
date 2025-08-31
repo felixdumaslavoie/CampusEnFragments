caec465cdee6a758976646d74bb3dfcafcded043
Félix Dumas-Lavoie <felixdumaslavoie@gmail.com>
2025-08-31 12:33:05 -0400

    Correctifs pour presentation.

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -39,7 +39,7 @@ export default function Navigation({ children }: { children: React.ReactNode; })
                                     Blog
                                 </MobileLink>
 
-                                <MobileLink href="/about">
+                                <MobileLink href="/apropos">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" aria-hidden>
                                         <title>Info Icon</title>
                                         <path fill="currentColor" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12c0 1.6.376 3.112 1.043 4.453c.178.356.237.763.134 1.148l-.595 2.226a1.3 1.3 0 0 0 1.591 1.592l2.226-.596a1.634 1.634 0 0 1 1.149.133A9.958 9.958 0 0 0 12 22" opacity=".5" />
@@ -71,7 +71,7 @@ export default function Navigation({ children }: { children: React.ReactNode; })
                                         <path fill="currentColor" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10" opacity=".5" />
                                         <path fill="currentColor" d="M8.97 8.97a.75.75 0 0 1 1.06 0L12 10.94l1.97-1.97a.75.75 0 1 1 1.06 1.06L13.06 12l1.97 1.97a.75.75 0 0 1-1.06 1.06L12 13.06l-1.97 1.97a.75.75 0 0 1-1.06-1.06L10.94 12l-1.97-1.97a.75.75 0 0 1 0-1.06" />
                                     </svg>
-                                    Close
+                                    Fermer
                                 </motion.button>
                             </div>
                         </motion.dialog>
